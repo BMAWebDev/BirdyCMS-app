fb55bba70ed6f2a1b02d8c3c1edba3c947263365
Bardita Andrei <bmihaiandrei@gmail.com>
2022-08-30 21:53:10 +0300

    configured axios

---
+++ lib/axios.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const Axios = axios.create({
+  baseURL: `${process.env.SERVER_URL}/api/`,
+  withCredentials: false,
+  timeout: 4000,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  },
+});
+
+interface success {
+  data: {
+    data: Array<Object>;
+    meta: Object;
+  };
+}
+
+interface error {
+  response: {
+    data: any;
+  };
+}
+
+const success = (res: success) => {
+  return res.data;
+};
+
+const error = (err: error) => {
+  return err.response.data;
+};
+
+Axios.interceptors.response.use(success, error);
+
+export default Axios;
